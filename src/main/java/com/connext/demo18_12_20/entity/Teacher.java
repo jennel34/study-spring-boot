d934544a9da48144f30e17fd4003ca1756d264e8
Jennel.Xu <860903648@qq.com>
2018-12-21 09:21:36 +0800

    测试

--- src/main/java/com/connext/demo18_12_20/entity/Teacher.java
+++ src/main/java/com/connext/demo18_12_20/entity/Teacher.java
@@ -1,4 +1,51 @@
 package com.connext.demo18_12_20.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 public class Teacher {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "teacher_name")
+    private String teacherName;
+
+    @Column(name = "teacher_age")
+    private int teacherAge;
+
+    @Column(name = "course_id")
+    private String courseID;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public int getTeacherAge() {
+        return teacherAge;
+    }
+
+    public void setTeacherAge(int teacherAge) {
+        this.teacherAge = teacherAge;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
 }
